63820ac87381dadde907e0e8a425848fdf092595
Evgenii Klimov <eklmv.spareacc@gmail.com>
2022-07-01 17:40:35 +0300

    solution and benchmark

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,8 @@
 package batch
 
 import (
+	"math"
+	"sync"
 	"time"
 )
 
@@ -14,5 +16,61 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	return getBatchSemaphore(n, pool)
+}
+
+// Goroutine per each request
+func getBatchSemaphore(n int64, pool int64) (res []user) {
+	var wg sync.WaitGroup
+	var lock sync.Mutex
+	sem := make(chan struct{}, pool)
+	for i := int64(0); i < n; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func(id int64) {
+			user := getOne(id)
+			lock.Lock()
+			res = append(res, user)
+			lock.Unlock()
+			<-sem
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return
+}
+
+type workerResult struct {
+	workerId int64
+	result   []user
+}
+
+//Goroutine per batch of requests
+func getBatchWorkers(n int64, pool int64) (res []user) {
+	producer := make(chan workerResult)
+	workerCapacity := int64(math.Ceil(float64(n) / float64(pool)))
+	for i := int64(0); i < pool; i++ {
+		go func(worker int64) {
+			startId := worker * workerCapacity
+			endId := (worker + 1) * workerCapacity
+			if worker == pool-1 {
+				endId = n
+			}
+			var batch []user
+			for id := startId; id < endId; id++ {
+				batch = append(batch, getOne(id))
+			}
+			producer <- workerResult{worker, batch}
+		}(i)
+	}
+
+	wr := make([]workerResult, pool)
+	for i := int64(0); i < pool; i++ {
+		r := <-producer
+		wr[r.workerId] = r
+	}
+	for _, r := range wr {
+		res = append(res, r.result...)
+	}
+	return
 }
--- lib/batch/batch_test.go
+++ lib/batch/batch_test.go
@@ -1,12 +1,26 @@
 package batch
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_getButch(t *testing.T) {
+func Test_getBatchSemaphore(t *testing.T) {
+	test(t, getBatchSemaphore)
+}
+
+func Test_getBatchWorkers(t *testing.T) {
+	test(t, getBatchWorkers)
+}
+
+func Test_GetBatch(t *testing.T) {
+	test(t, getBatch)
+}
+
+func test(t *testing.T, target func(int64, int64) []user) {
 	type args struct {
 		n    int64
 		pool int64
@@ -22,12 +36,13 @@ func Test_getButch(t *testing.T) {
 		{args: args{n: 100, pool: 10}, wantRes: createRes(100)},
 		{args: args{n: 15, pool: 5}, wantRes: createRes(15)},
 		{args: args{n: 35, pool: 5}, wantRes: createRes(35)},
+		{args: args{n: 100, pool: 6}, wantRes: createRes(100)},
 	}
 	for _, tt := range tests {
 		t.Run("", func(t *testing.T) {
 			start := time.Now()
 			wantTime := start.Add(time.Duration(tt.args.n/tt.args.pool) * 100)
-			actualRes := getBatch(tt.args.n, tt.args.pool)
+			actualRes := target(tt.args.n, tt.args.pool)
 			since := time.Since(start).Milliseconds()
 			assert.WithinDuration(t, wantTime, start.Add(time.Duration(since)), time.Nanosecond*200)
 			assert.ElementsMatch(t, tt.wantRes, actualRes)
@@ -41,4 +56,34 @@ func createRes(v int64) []user {
 		res = append(res, user{ID: int64(i)})
 	}
 	return res
+}
+
+// benchmarking
+
+var input = []struct {
+	n    int64
+	pool int64
+}{
+	{100000, 6},
+	{100000, 12},
+	{100000, 24},
+	{100000, 56},
+}
+
+func benchmarkOverInput(b *testing.B, target func(int64, int64) []user) {
+	for _, in := range input {
+		b.Run(fmt.Sprintf("n_%d_pool_%d", in.n, in.pool), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				target(in.n, in.pool)
+			}
+		})
+	}
+}
+
+func Benchmark_getBatchSemaphore(b *testing.B) {
+	benchmarkOverInput(b, getBatchSemaphore)
+}
+
+func Benchmark_getBatchWorkers(b *testing.B) {
+	benchmarkOverInput(b, getBatchWorkers)
 }
